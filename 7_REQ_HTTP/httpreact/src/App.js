da9ebea51cc5c91366d15c875b3d6477ea181d70
PatrickAlmeidaLima <patrick.almeida.lima@gmail.com>
2022-09-04 21:45:59 -0300

    Aula 7 requisições de bd json

---
+++ 7_REQ_HTTP/httpreact/src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Lista de produtos</h1>
+    </div>
+  );
+}
+
+export default App;
+
+
+//npm run server
+// npm start
