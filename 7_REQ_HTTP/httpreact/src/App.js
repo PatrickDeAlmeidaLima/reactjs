ba5d89e1d451fd908511a617f6bf393a5aca971b
PatrickAlmeidaLima <patrick.almeida.lima@gmail.com>
2022-09-08 21:19:09 -0300

    Aula 5 refatorando post e aula 4 criando custom hook

--- 7_REQ_HTTP/httpreact/src/App.js
+++ 7_REQ_HTTP/httpreact/src/App.js
@@ -1,20 +1,28 @@
 
 import './App.css';
 import { useState, useEffect } from "react";
+// 4 - custom hook
+import { useFetch } from './hooks/useFetch';
 const url = "http://localhost:3000/products";
 function App() {
   const [products, setProducts] = useState([]);
   const [name, setName] = useState("");
   const [price, setPrice] = useState("");
+  // 4 - custom hook
+  const { data: items, httpConfig } = useFetch(url);
+
+  // console.log(data);
   // 1 - resgatando dados
-  useEffect(() => {
-    async function fetchData() {
-      const res = await fetch(url);
-      const data = await res.json();
-      setProducts(data);
-    }
-    fetchData();
-  }, []);
+  // 4 - colocando custom hook
+  // useEffect(() => {
+  //   async function fetchData() {
+  //     const res = await fetch(url);
+  //     const data = await res.json();
+  //     setProducts(data);
+  //   }
+  //   fetchData();
+  // }, []);
+  // 4 - colocando custom hook
 
   // 2 - adicionando produtos
   const handleSubmit = async (e) => {
@@ -23,16 +31,19 @@ function App() {
       name,
       price,
     };
-    const res = await fetch(url, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify(products),
-    });
-    // 3 - carregamento dinâmico
-    const addedProducts = await res.json()
-    setProducts((prevProducts) => [...prevProducts, addedProducts])
+    // const res = await fetch(url, {
+    //   method: "POST",
+    //   headers: {
+    //     "Content-Type": "application/json"
+    //   },
+    //   body: JSON.stringify(products),
+    // });
+    // // 3 - carregamento dinâmico
+    // const addedProducts = await res.json()
+    // setProducts((prevProducts) => [...prevProducts, addedProducts])
+
+    // 5 - refatorando post
+    httpConfig(products, "POST")
     setName("");
     setPrice("");
   };
@@ -41,7 +52,7 @@ function App() {
     <div className="App">
       <h1>Lista de produtos</h1>
       <ul>
-        {products.map((product) => (
+        {items && items.map((product) => (
           <li key={product.id}>{product.name} - R$:{product.price}</li>
         ))}
       </ul>
