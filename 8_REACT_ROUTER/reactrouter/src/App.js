0608d76aa9e737203edbeb5bc3478c2dde1fcd7e
PatrickAlmeidaLima <patrick.almeida.lima@gmail.com>
2022-09-13 20:55:50 -0300

    Arrumando routes para navegação sem trocar de página

--- 8_REACT_ROUTER/reactrouter/src/App.js
+++ 8_REACT_ROUTER/reactrouter/src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
 
+// 1 - config react router
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+// pages
+import Home from "./pages/Home";
+import About from "./pages/About";
+// components
+import Navbar from './components/Navbar';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>React router</h1>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ 8_REACT_ROUTER/reactrouter/src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import "./Navbar.css"
+const Navbar = () => {
+    return (
+        <nav>
+            <Link to="/">Home</Link>
+            <Link to="/About">About</Link>
+        </nav>
+    )
+}
+
+export default Navbar
