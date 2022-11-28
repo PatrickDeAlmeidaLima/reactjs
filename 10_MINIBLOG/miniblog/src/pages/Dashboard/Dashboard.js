25dc1de1937b2ced445d7752764a6f972a373531
PatrickAlmeidaLima <patrick.almeida.lima@gmail.com>
2022-11-28 20:18:15 -0300

    Estruturando dashboard

--- 10_MINIBLOG/miniblog/src/pages/Dashboard/Dashboard.js
+++ 10_MINIBLOG/miniblog/src/pages/Dashboard/Dashboard.js
@@ -1,9 +1,36 @@
-import React from 'react'
 import styles from './Dashboard.modules.css'
+
+import { Link } from 'react-router-dom'
+
+// hooks
+
+import { useAuthValue } from '../../context/AuthContent';
+import { useFetchDocument } from '../../hooks/useFetchDocument'
+
+
+
 const Dashboard = () => {
+  const { user } = useAuthValue();
+  const uid = user.uid;
+
+  //post do usuário
+  const posts = [];
+
+
   return (
     <div>
-        <h1>Dashboard</h1>
+      <h2>Dashboard</h2>
+      <p>Gerencie os seus posts</p>
+      {posts && posts.length === 0 ? (
+        <div className={styles.noposts}>
+          <p>Não foram encontrados posts!</p>
+          <Link to="/posts/create" className='btn'>Criar primeiro post!</Link>
+        </div>
+      ) : (
+        <div>
+          <p>Tem posts!</p>
+        </div>
+      )}
     </div>
   )
 }
--- 10_MINIBLOG/miniblog/src/pages/Post/Post.js
+++ 10_MINIBLOG/miniblog/src/pages/Post/Post.js
@@ -11,11 +11,19 @@ const Post = () => {
     const { document: post, loading } = useFetchPost("posts", id)
 
     return (
-        <div>
+        <div className={styles.post_container}>
             {loading && <p>Carregando post...</p>}
             {post && (
                 <>
                     <h1>{post.title}</h1>
+                    <img src={post.image} alt={post.title} />
+                    <p>{post.body}</p>
+                    <h3>Este post trata sobre:</h3>
+                    <div className={styles.tags}>
+                        {post.tagsArray.map((tag) => (
+                            <p key={tag}><span>#</span>{tag}</p>
+                        ))}
+                    </div>
                 </>
             )}
         </div>
