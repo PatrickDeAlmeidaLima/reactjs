4eb9b3d4b502de0caa55cf9abb66b83054597036
PatrickAlmeidaLima <patrick.almeida.lima@gmail.com>
2022-12-20 21:08:39 -0300

    CSS dos detalhes do post

--- 10_MINIBLOG/miniblog/src/components/PostDetail.js
+++ 10_MINIBLOG/miniblog/src/components/PostDetail.js
@@ -4,17 +4,17 @@ import { Link } from "react-router-dom";
 
 const PostDetail = ({ post }) => {
     return (
-        <div>
+        <div className={styles.post_detail}>
             <img src={post.image} alt={post.title} />
             <h2>{post.title}</h2>
-            <p>{post.createdBy}</p>
-            <div>
+            <p className={styles.createdBy}>{post.createdBy}</p>
+            <div className={styles.tags}>
                 {post.tagsArray.map((tag) => (
                     <p key={tag}><span>#</span>{tag}</p>
                 ))}
             </div>
             <Link to={`/posts/${post.id}`} className="btn btn-outline">Ler</Link>
-        </div>
+        </div >
     )
 }
 
