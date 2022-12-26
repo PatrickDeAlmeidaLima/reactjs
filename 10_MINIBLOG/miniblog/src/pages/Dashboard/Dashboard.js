a005b73b1b5c71ca6c9a098d6bf1967bf8fd5488
PatrickAlmeidaLima <patrick.almeida.lima@gmail.com>
2022-12-26 20:16:49 -0300

    Estruturando dashboard

--- 10_MINIBLOG/miniblog/src/pages/Dashboard/Dashboard.js
+++ 10_MINIBLOG/miniblog/src/pages/Dashboard/Dashboard.js
@@ -1,11 +1,36 @@
-import styles from './Dashboard.module.css'
+import styles from './Dashboard.module.css';
+
+import { Link } from 'react-router-dom';
+
+//hooks
+import { useAuthValue } from '../../context/AuthContext';
+import { useFetchDocuments } from '../../hooks/useFetchDocuments';
+
 
 const Dashboard = () => {
+
+    const { user } = useAuthValue();
+    const uid = user.uid;
+
+    // posts do usuário
+    const posts = [];
+
+
+
     return (
         <div>
-            <h1>
-                Dashboard
-            </h1>
+            <h2>Dashboard</h2>
+            <p>Gerencie os seus posts</p>
+            {posts && posts.length === 0 ? (
+                <div className={styles.noposts}>
+                    <p>Não foram encontrados posts</p>
+                    <Link to="/posts/create" className="btn">Criar primeiro Post</Link>
+                </div>
+            ) : (
+                <div>
+                    Tem Posts!
+                </div>
+            )}
         </div>
     )
 }
