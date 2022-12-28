da999fea4f4d6eb12524c89779be4bf211ed4fcb
PatrickAlmeidaLima <patrick.almeida.lima@gmail.com>
2022-12-28 20:26:18 -0300

    Excluindo post na dashboard

--- 10_MINIBLOG/miniblog/src/pages/Dashboard/Dashboard.js
+++ 10_MINIBLOG/miniblog/src/pages/Dashboard/Dashboard.js
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom';
 //hooks
 import { useAuthValue } from '../../context/AuthContext';
 import { useFetchDocuments } from '../../hooks/useFetchDocuments';
-
+import { useDeleteDocument } from '../../hooks/useDeleteDocument';
 
 const Dashboard = () => {
 
@@ -14,9 +14,8 @@ const Dashboard = () => {
 
     const { documents: posts, loading } = useFetchDocuments("posts", null, uid);
 
-    const deleteDocument = (id) => {
+    const { deleteDocument } = useDeleteDocument("posts")
 
-    }
     if (loading) {
         return <p>Carregando...</p>
     }
