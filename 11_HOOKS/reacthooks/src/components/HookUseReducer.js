03485e3d19a9545738faf97586b9d7c4f56d1b6c
PatrickAlmeidaLima <patrick.almeida.lima@gmail.com>
2023-01-05 20:27:49 -0300

    useReducer com actions

--- 11_HOOKS/reacthooks/src/components/HookUseReducer.js
+++ 11_HOOKS/reacthooks/src/components/HookUseReducer.js
@@ -1,4 +1,4 @@
-import { useReducer } from 'react'
+import { useReducer, useState } from 'react'
 
 const HookUseReducer = () => {
     // 1 - começando com useReducer
@@ -6,11 +6,51 @@ const HookUseReducer = () => {
         return Math.random(state);
     });
 
+    // 2 - avançando no useReducer
+    const initialTasks = [
+        { id: 1, text: "Aprendendo mais sobre o useReducer." },
+        { id: 2, text: "Aprendendo mais sobre o reactJS" }
+    ]
+    const taskReducer = (state, action) => {
+        switch (action.type) {
+            case "ADD":
+                const newTask = {
+                    id: Math.random(),
+                    text: taskText
+                }
+                setTaskText("")
+                return [...state, newTask];
+            case "DELETE":
+                return state.filter((task) => task.id !== action.id)
+            default:
+                return state;
+        }
+    };
+
+    const [taskText, setTaskText] = useState("");
+    const [tasks, dispatchTasks] = useReducer(taskReducer, initialTasks);
+    const handleSubmit = (e) => {
+        e.preventDefault()
+
+        dispatchTasks({ type: "ADD" })
+    }
+
+    const removeTask = (id) => {
+        dispatchTasks({ type: "DELETE", id });
+    }
     return (
         <div>
             <h2>useReducer</h2>
             <p>Número : {number}</p>
             <button onClick={dispatch}>Alterar número</button>
+            <h3>Tarefas :</h3>
+            <form onSubmit={handleSubmit}>
+                <input type="text" onChange={(e) => setTaskText(e.target.value)} value={taskText} />
+                <input type="submit" value="Enviar" />
+            </form>
+            {tasks.map((task) => (
+                <li key={task.id} onDoubleClick={() => removeTask(task.id)}>{task.text}</li>
+            ))}
         </div>
     )
 }
